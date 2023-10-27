a47ea458bfaf43682168b9ee7b9538c170e70742
Pavel Valger <pavelwalker@mail.ru>
2023-10-27 19:23:05 +0500

    4. Конфигурация контейнера [#505072 #455918]

---
+++ src/main/java/ru/job4j/di/annotation/Config.java
@@ -0,0 +1,29 @@
+package ru.job4j.di.annotation;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@ComponentScan(basePackages = "ru/job4j/di/annotation")
+@PropertySource("classpath:application.properties")
+public class Config {
+
+    @Bean
+    @Primary
+    @Order(11)
+    public Dog getAlfaDog() {
+        return new Dog("AlfaDog");
+    }
+
+    @Bean(name = "SecondDog")
+    @Order(2)
+    public Dog getDog() {
+        return new Dog("SecondDog");
+    }
+
+    @Bean
+    @Order(7)
+    public Dog someDog() {
+        return new Dog("Dog");
+    }
+}
