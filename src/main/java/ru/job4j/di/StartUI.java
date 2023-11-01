9a6608ed16c62215411f3c25c4be62f84c95e72d
Pavel Valger <pavelwalker@mail.ru>
2023-11-01 21:25:54 +0500

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,15 +1,18 @@
 package ru.job4j.di;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class StartUI {
+    private final Store store;
+    private final ConsoleInput consoleInput;
 
-    @Autowired
-    private Store store;
-    @Autowired
-    private ConsoleInput consoleInput;
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
 
     public void add(String value) {
         store.add(value);
